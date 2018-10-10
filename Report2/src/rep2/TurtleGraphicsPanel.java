32f36702a52972e8ded528969a802f1d8ae7a286
6317028 <6317028@nodab011.ed.tus.ac.jp>
2018-10-10 14:31:28 +0900

    added ArrayList turtles

--- Report2/src/rep2/TurtleGraphicsPanel.java
+++ Report2/src/rep2/TurtleGraphicsPanel.java
@@ -1,12 +1,14 @@
 package rep2;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 public class TurtleGraphicsPanel extends JPanel {
 	//Turtle インスタンス
 	private Turtle kame;
+	private ArrayList<Turtle> turtles = new ArrayList<Turtle>();
 	
 	//コンストラクタ
 	TurtleGraphicsPanel(){
