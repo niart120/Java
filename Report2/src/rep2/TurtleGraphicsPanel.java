86eee96086e85491522d3ca327d32dff8f0db770
6317028 <6317028@nodab011.ed.tus.ac.jp>
2018-10-10 14:28:23 +0900

    added field variable (Turtle kame)

--- Report2/src/rep2/TurtleGraphicsPanel.java
+++ Report2/src/rep2/TurtleGraphicsPanel.java
@@ -3,5 +3,11 @@ package rep2;
 import javax.swing.JPanel;
 
 public class TurtleGraphicsPanel extends JPanel {
-
+	//Turtle インスタンス
+	private Turtle kame;
+	
+	//コンストラクタ
+	TurtleGraphicsPanel(){
+		
+	}
 }
