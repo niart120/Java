909c3c9d649eccd185d19ded307ed6f4af2acc39
6317028 <6317028@nodab011.ed.tus.ac.jp>
2018-10-10 14:38:41 +0900

    implemented main method

--- Report2/src/rep2/TurtleGraphics.java
+++ Report2/src/rep2/TurtleGraphics.java
@@ -1,9 +1,23 @@
 package rep2;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 public class TurtleGraphics {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		JFrame fr = new JFrame("TurtleGraphics");
+	    
+	    fr.setSize(400,400);
+	    fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    fr.getContentPane().setBackground(new Color(255, 255, 255));
+
+	    TurtleGraphicsPanel panel = new TurtleGraphicsPanel();
+	    panel.setOpaque(false);
+	    fr.add(panel);
+
+	    fr.setVisible(true);
 
 	}
 
