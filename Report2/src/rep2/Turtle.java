4f0bd2afde1e8a0a422dd83026ee79b1b841b0e2
6317028 <6317028@nodab011.ed.tus.ac.jp>
2018-10-10 13:31:51 +0900

    added some field variable (pos, angle, color, pensize)

--- Report2/src/rep2/Turtle.java
+++ Report2/src/rep2/Turtle.java
@@ -1,5 +1,18 @@
 package rep2;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Point;
+
 public class Turtle {
 
+	//現在位置
+	Point presentPos;
+	//向き
+	double angle;
+	//ペンの色
+	Color penColor;
+	//ペンサイズ
+	BasicStroke penSize;
+	
 }
