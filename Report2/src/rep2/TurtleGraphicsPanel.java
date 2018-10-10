795198335b6fb09a6bd9178416df5d46b3070c66
niart120(home) <axe0120@gmail.com>
2018-10-10 22:02:59 +0900

    added mouse event (Clicked, Dragged)

--- Report2/src/rep2/TurtleGraphicsPanel.java
+++ Report2/src/rep2/TurtleGraphicsPanel.java
@@ -11,21 +11,24 @@ import javax.swing.JPanel;
 public class TurtleGraphicsPanel extends JPanel implements MouseListener, MouseMotionListener{
 	//Turtle インスタンス
 	private Turtle kame;
-	
+
 	//クリック時の座標保持
-	private int pressedx;
-	private int pressedy;
-	
+	private int pressedX;
+	private int pressedY;
+
+	//選択された亀のリスト番号(-1=無選択)
+	private int selectedTurtle = -1;
+
 	//Turtle保持用のArrayList
 	private ArrayList<Turtle> turtles = new ArrayList<Turtle>();
-	
+
 	//コンストラクタ
 	TurtleGraphicsPanel(){
 		kame = new Turtle(200,200);
 		regPolygons(kame,40,15);
 		turtles.add(kame);
 	}
-	
+
 	//Turtleに正方形を描かせる
 	void square(Turtle t) {
 		for(int i=0;i<4;i++) {
@@ -33,7 +36,7 @@ public class TurtleGraphicsPanel extends JPanel implements MouseListener, MouseM
 			t.turn(90);
 		}
 	}
-	
+
 	//Turtleに正三角形を描かせる
 	void triangle(Turtle t, int size) {
 		t.move(size);
@@ -51,7 +54,7 @@ public class TurtleGraphicsPanel extends JPanel implements MouseListener, MouseM
 			t.turn(d);
 		}
 	}
-	
+
 	//Turtleに正多角形を描かせる
 	void regPolygon(Turtle t, int size, int n) {
 		//外角計算
@@ -62,7 +65,7 @@ public class TurtleGraphicsPanel extends JPanel implements MouseListener, MouseM
 			t.turn(dgr);
 		}
 	}
-	
+
 	//Turtleにn個の正(n+2)角形を描かせる
 	void regPolygons(Turtle t, int size, int n) {
 		if(3<=n) {
@@ -81,36 +84,59 @@ public class TurtleGraphicsPanel extends JPanel implements MouseListener, MouseM
 
 	@Override
 	public void mouseDragged(MouseEvent arg0) {
-		
+		if(selectedTurtle != -1) {
+			//相対距離の取得
+			int dx = arg0.getX()-pressedX;
+			int dy = arg0.getY()-pressedY;
+
+			//オフセット値の更新
+			turtles.get(selectedTurtle).setOffset(dx,dy);
+
+			//マウス位置の更新
+			pressedX = dx;
+			pressedY = dy;
+		}
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent arg0) {
-		
+
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		
+		for(int i=0;i<turtles.size();i++) {
+			for(Section sec: turtles.get(i).paths) {
+				//経路にマウス座標が含まれていればそのTurtleを選択
+				if(sec.isInPoint(arg0.getX(), arg0.getY())) {
+					selectedTurtle = i;
+					pressedX = arg0.getX();
+					pressedY = arg0.getY();
+					return;
+				}
+			}
+		}
+		//上記の処理を抜けたならば未選択として処理
+		selectedTurtle = -1;
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
-		
+
 	}
 }
