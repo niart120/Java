e20dd0c002ce6f5ae632ccbc4f174f054a30c654
6317028 <6317028@nodab011.ed.tus.ac.jp>
2018-10-10 14:35:21 +0900

    added square method

--- Report2/src/rep2/TurtleGraphicsPanel.java
+++ Report2/src/rep2/TurtleGraphicsPanel.java
@@ -8,15 +8,26 @@ import javax.swing.JPanel;
 public class TurtleGraphicsPanel extends JPanel {
 	//Turtle インスタンス
 	private Turtle kame;
+	
+	//Turtle保持用のArrayList
 	private ArrayList<Turtle> turtles = new ArrayList<Turtle>();
 	
 	//コンストラクタ
 	TurtleGraphicsPanel(){
 		kame = new Turtle(200,200);
+		turtles.add(kame);
+	}
+	
+	//Turtleに正方形を描かせる
+	void square(Turtle t) {
+		t.move(50);
+		t.turn(90);
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
-		
+		for(Turtle t: turtles) {
+			t.paint(g);
+		}
 	}
 }
